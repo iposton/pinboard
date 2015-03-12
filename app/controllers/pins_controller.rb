a7b5d34269a67398a9be38dc22b2d97f271603f6
iposton <ianposton@sbcglobal.net>
2015-03-11 21:29:59 -0700

    fix error heroku

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,7 +6,7 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 3
+    @pins = Pin.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
   end
 
   # GET /pins/1
